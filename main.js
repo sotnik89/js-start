af1b78eda2449bb7dff09e7dec4867d61ac21358
sotnik89 <sotnik89@gmail.com>
2025-06-20 17:21:43 +0300

    231c5

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-// let number = 7.6
-// console.log(Math.round(7.6))
-// let text = ('“Hello, world!”');
-// let characterCount = text.length;
-// console.log(characterCount);
+let number = 7.6
+console.log(Math.round(7.6))
+let text = ('“Hello, world!”');
+let characterCount = text.length;
+console.log(characterCount);
 
-let string = ('Hello, world!');
-let characterCount = string.length;
-export{characterCount};
+// let string = ('Hello, world!');
+// let characterCount = string.length;
+// export{characterCount};
